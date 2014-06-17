fc2f35151ca51ac55d5ae9738224f02198b285c0
Ramit Arora <ravic@biz2credit.com>
2014-06-17 07:26:51 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2CreditApp/scripts/schedule.js
+++ Biz2CreditApp/scripts/schedule.js
@@ -100,6 +100,7 @@
     });
     $(window).on('orientationchange', function () {
       $("#tabstrip-home").find(".km-scroll-container").css("-webkit-transform", "translate3d(0px, 0px, 0px)");
+      $("#tabstrip-sign-up").find(".km-scroll-container").css("-webkit-transform", "translate3d(0px, 0px, 0px)");
       app.homesetting.viewModel.closeParentPopover();
       
     });
